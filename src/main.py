50baf87a140fe8a449d27a0bb2c79d90d4378515
michael <7471919@naver.com>
2025-03-15 15:15:51 +0900

    fix: 환경변수 세팅 로직 변경

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
+import os
 from fastapi import FastAPI
 from src.config import Global
 from src.contorllers.index import index_router
+
+Global.validate_env()
 
 app = FastAPI(
     docs_url="/api-docs",
 )
 
 app.include_router(index_router)
-
-Global.validate_env()
 
 print(Global.env.DATABASE_URL)
 
