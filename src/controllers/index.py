3aea1213bd007713578ddfb398759107096b08b6
jjaegii <hn06038@gmail.com>
2025-03-30 19:14:14 +0900

    feat: add chart analysis router to main router

--- src/controllers/index.py
+++ src/controllers/index.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from src.contorllers.agent import agent_router
 from src.contorllers.kis import kis_router
+from src.contorllers.chartAnalysis import chart_analysis_router
 
 index_router = router = APIRouter(prefix="/api")
 
 index_router.include_router(agent_router)
 index_router.include_router(kis_router)
+index_router.include_router(chart_analysis_router)
