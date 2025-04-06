a77b0c24347f00841923b19b9e03bd767102a8cd
jjaegii <hn06038@gmail.com>
2025-04-06 22:46:14 +0900

    refactor: Update router configuration
    - Remove chart_analysis_router import
    - Update router configuration to use agent_router for chart analysis

--- src/controllers/index.py
+++ src/controllers/index.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter
 from src.controllers.agent import agent_router
 from src.controllers.kis import kis_router
-from src.controllers.chartAnalysis import chart_analysis_router
 
 index_router = router = APIRouter(prefix="/api")
 
 index_router.include_router(agent_router)
 index_router.include_router(kis_router)
-index_router.include_router(chart_analysis_router)
